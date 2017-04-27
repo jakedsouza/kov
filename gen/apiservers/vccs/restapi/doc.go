e4d84f8e4e70ecebe92abbb508fdcc5d36d1017a
kreamyx <karim.y.elghamrawy@gmail.com>
2017-04-27 16:45:50 -0700

    added generated code

---
+++ gen/apiservers/vccs/restapi/doc.go
@@ -0,0 +1,23 @@
+/*Package restapi API Specification for the vSphere Container Cluster Service (VCCS)
+
+# RESTful API for the vSphere Container Cluster Service (VCCS)
+
+
+
+    Schemes:
+      https
+    Host: localhost
+    BasePath: /
+    Version: 0.2.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
