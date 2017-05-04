515f2ff5330e38b953f9b7501336b76a8a531313
Jake Dsouza <jaked@vmware.com>
2017-05-04 11:04:51 -0700

    Run glide update
    - Ran glide update --skip-test -v

--- vendor/github.com/mattn/go-isatty/_example/example.go
+++ vendor/github.com/mattn/go-isatty/_example/example.go
@@ -1,4 +1,4 @@
-package isatty_test
+package main
 
 import (
 	"fmt"
@@ -7,7 +7,7 @@ import (
 	"github.com/mattn/go-isatty"
 )
 
-func Example() {
+func main() {
 	if isatty.IsTerminal(os.Stdout.Fd()) {
 		fmt.Println("Is Terminal")
 	} else if isatty.IsCygwinTerminal(os.Stdout.Fd()) {
